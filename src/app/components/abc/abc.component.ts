4c49fa9eec550d882769a47af039acde067ed640
Mohit-S-Rane <mohitsrane16@gmail.com>
2023-06-13 12:33:55 +0530

    Simple Component creation done

---
+++ src/app/components/abc/abc.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-abc',
+    templateUrl: './abc.component.html',
+    styleUrls: ['./abc.component.scss']
+})
+
+export class AbcComponent {
+    
+}

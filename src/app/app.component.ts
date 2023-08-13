ca5362f9f36ef2e64ead6b53779d2774cb0140ce
MSRane <mohitsrane16@gmail.com>
2023-08-13 10:58:29 +0530

    Behavior Subject added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { filter, map, Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, filter, map, Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,47 +10,25 @@ import { filter, map, Observable, switchMap } from 'rxjs';
 export class AppComponent {
   title = 'angular-course';
   loginForm: FormGroup
+  mySubject = new BehaviorSubject('hello')
 
   constructor() {
     this.loginForm = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.maxLength(8), Validators.minLength(2)])
     })
-    this.buySugarFromShop()
   }
-
-  buySugerInBulk () {
-    return new Observable(emitter => {
-      emitter.next('Sugar is purchased..')
-    })
-  }
-
-  buySugarInQuantity(quantity:any) {
-    return new Observable(emitter => {
-      emitter.next('Sugar with Quantity: ' + quantity + ' is here for you')
-    })
-  }
-
-  buySugarFromShop(){
-    const newObserver = this.buySugerInBulk().pipe(switchMap(()=> {
-      return this.buySugarInQuantity('1kg')
-    }))
-  
-    newObserver.subscribe((data)=>{
-      console.log(data);
-      
-    })
-  
-  
-  }
-
-
-
-
-
 
   login() {
+    this.mySubject.next(this.loginForm.value)
   }
 
-  signup() { }
+  signup() { 
+    // this.mySubject.subscribe((data)=>{
+    //   console.log(data);
+    // })
+
+    console.log(this.mySubject.getValue());  
+    // Get value return last value
+  }
 }

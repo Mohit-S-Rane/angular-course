f248bf543a2c2d80a56e0ff09d2eee4d544d58e1
MSRane <mohitsrane16@gmail.com>
2023-08-13 11:06:50 +0530

    Combine Latest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, filter, map, Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, combineLatest, filter, map, Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,25 +10,28 @@ import { BehaviorSubject, filter, map, Observable, switchMap } from 'rxjs';
 export class AppComponent {
   title = 'angular-course';
   loginForm: FormGroup
-  mySubject = new BehaviorSubject('hello')
 
   constructor() {
     this.loginForm = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.maxLength(8), Validators.minLength(2)])
     })
+    const observableA = this.loginForm.valueChanges;
+    const observableB = new Observable((emitter) =>{
+      emitter.next('hello')
+    })
+    
+  // combine latest used to combine more than observable and listen latest value 
+    combineLatest([observableA, observableB]).subscribe((data)=>{
+      console.log(data);
+    })
   }
 
   login() {
-    this.mySubject.next(this.loginForm.value)
+
   }
 
   signup() { 
-    // this.mySubject.subscribe((data)=>{
-    //   console.log(data);
-    // })
 
-    console.log(this.mySubject.getValue());  
-    // Get value return last value
   }
 }

edf1cc753dc7bbadac12d94cb9e725e1cb5a8add
MSRane <mohitsrane16@gmail.com>
2023-08-13 11:14:18 +0530

    Debounce time and distinctUntilChange

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { BehaviorSubject, combineLatest, filter, map, Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, combineLatest, debounceTime, distinctUntilChanged, filter, map, Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,14 +16,10 @@ export class AppComponent {
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.maxLength(8), Validators.minLength(2)])
     })
-    const observableA = this.loginForm.valueChanges;
-    const observableB = new Observable((emitter) =>{
-      emitter.next('hello')
-    })
-    
-  // combine latest used to combine more than observable and listen latest value 
-    combineLatest([observableA, observableB]).subscribe((data)=>{
+    const observer = this.loginForm.valueChanges.pipe(map(data => data.email), debounceTime(500), distinctUntilChanged())
+    observer.subscribe((data)=>{
       console.log(data);
+      
     })
   }
 

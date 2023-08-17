051c9395694db7ad9e6877936070ed470f4e45dd
MSRane <mohitsrane16@gmail.com>
2023-08-17 21:50:26 +0530

    logic for completed step

--- src/app/container/on-boarding/resume-name/resume-name.component.ts
+++ src/app/container/on-boarding/resume-name/resume-name.component.ts
@@ -8,18 +8,21 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 })
 
 export class ResumeNameComponent implements OnInit {
-    resumeForm: FormGroup;
-  
-    constructor() {
-    }
-  
-    ngOnInit() {
-      this.resumeForm = new FormGroup({
-        name: new FormControl(null, [Validators.required])
-      });
-    }
-  
-    createResume() {
-      console.log('called');
-    }
+  resumeForm: FormGroup;
+  isCompleted = true;
+  loading = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.resumeForm = new FormGroup({
+      name: new FormControl(null, [Validators.required])
+    });
+  }
+
+  createResume() {
+    // actual m data ko save kro using an api
+    this.isCompleted = true;
+  }
   }
